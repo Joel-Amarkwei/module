f8e4420ec871f96758d228d21177a541708cbcda
Joel Amarkwei <joel.amarkwei@gmail.com>
2020-02-19 17:28:02 +0000

    Beginning of the Beginning

---
+++ module/TodoListViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  module
+//
+//  Created by The Remedy on 19/02/2020.
+//  Copyright © 2020 The Remedy. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Mandacolos", "Shemacolos", "Fletcherolos"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+
+}
+
