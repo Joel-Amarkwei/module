68d9b8f74fcd1e3fe178c23e95f37611b9558d10
Joel Amarkwei <joel.amarkwei@gmail.com>
2020-02-28 14:03:09 +0000

    Installing RealmSwift

--- module/Controller/CategoryViewController.swift
+++ module/Controller/CategoryViewController.swift
@@ -43,7 +43,7 @@ class CategoryViewController: UITableViewController {
         
         performSegue(withIdentifier: "goToItems", sender: self)
         
-     //   tableView.deselectRow(at: indexPath, animated: true)
+        tableView.deselectRow(at: indexPath, animated: true)
         
     }
     
